4ca8d51e8c3f540d666b0e8001aa770a33ad99af
sst793 <steven.s.tumino@sherwin.com>
2025-06-23 13:57:32 -0400

    fixed error

--- appThing.java
+++ appThing.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         // Your program's logic starts here
-        System.out.println("Hello, Java!");
+        System.out.println("Hello, Java!!!");
     }
 }
